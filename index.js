deb29eb974ef0a594b87f3f9441b35d4934dae53
Leonard <lmuseau@gmail.com>
2020-11-28 19:10:51 -0600

    reverted changes

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const mongoose = require('mongoose');
 
 const typeDefs = require('./graphql/typeDefs');
 const resolvers = require('./graphql/resolvers')
-// const { MONGODB } = require('./config.js');
-const { MONGODB } = process.env.MONGODB;
+const { MONGODB } = require('./config.js');
 
 const pubsub = new PubSub();
 
